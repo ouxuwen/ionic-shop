4ab2b2053c62723d5dd82598fad40b0b3d4a934a
ouxuwen <mo22398291@163.com>
2018-08-09 01:06:51 +0800

    update icon

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,12 +2,12 @@ const DEPLOY = "debug";
 
 const FETCH = {
   "production": {
-    urlPrefix: 'http://r7miit.natappfree.cc/ionic-shop/index.php',
-    imgPrefix: 'http://r7miit.natappfree.cc/ionic-shop/'
+    urlPrefix: 'http://127.0.0.1:8080/ionic-shop/index.php',
+    imgPrefix: 'http://127.0.0.1:8080/ionic-shop/'
   },
   "debug": {
-    urlPrefix: 'http://r7miit.natappfree.cc/ionic-shop/index.php',
-    imgPrefix: 'http://r7miit.natappfree.cc/ionic-shop/'
+    urlPrefix: 'http://127.0.0.1:8080/ionic-shop/index.php',
+    imgPrefix: 'http://127.0.0.1:8080/ionic-shop/'
   }
 }
 

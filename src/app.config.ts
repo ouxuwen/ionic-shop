9f832d128f003cc6da88203e7c34522fae81e27e
ouxuwen <mo22398291@163.com>
2018-08-27 21:44:05 +0800

    update

--- src/app.config.ts
+++ src/app.config.ts
@@ -6,8 +6,8 @@ const FETCH = {
     imgPrefix: 'http://120.79.209.150/ionic-shop/'
   },
   "debug": {
-    urlPrefix: 'http://120.79.209.150/ionic-shop/index.php',
-    imgPrefix: 'http://120.79.209.150/ionic-shop/'
+    urlPrefix: 'http://pzdu48.natappfree.cc/ionic-shop/index.php',
+    imgPrefix: 'http://pzdu48.natappfree.cc/ionic-shop/'
   }
 }
 
